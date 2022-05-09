3814f3dd0e6ec4d5f9dcfb19d7f2a5e030354843
Shengyou Fan <shengyoufan@gmail.com>
2022-05-09 23:53:29 +0800

    add compareBy() example

--- src/main/kotlin/io/kraftsman/collection/technique/ordering/sortedWith.ws.kts
+++ src/main/kotlin/io/kraftsman/collection/technique/ordering/sortedWith.ws.kts
@@ -9,9 +9,21 @@ val target = listOf("aaa", "bb", "c")
 val lengthComparator = Comparator { str1: String, str2: String ->
     str1.length - str2.length
 }
+val warehouse = listOf(
+    "Apple" to 123,
+    "Papaya" to 8,
+    "Orange" to 72,
+    "Grape" to 21,
+    "Banana" to 205,
+    "Pineapple" to 47
+)
 
 // 測試 APIs
 target.sortedWith(lengthComparator)
 
 target.sortedWith(compareBy { it.length })
 target.sortedWith(compareByDescending { it.length })
+
+warehouse.sortedWith(
+    compareBy({ it.first }, { it.second })
+)
