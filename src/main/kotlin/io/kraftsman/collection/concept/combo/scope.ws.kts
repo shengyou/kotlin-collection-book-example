1826d9a94a0f81a5926d16046567c0c52c3ffb80
Shengyou Fan <shengyoufan@gmail.com>
2022-05-24 15:55:51 +0800

    update example for scope functions

--- src/main/kotlin/io/kraftsman/collection/concept/combo/scope.ws.kts
+++ src/main/kotlin/io/kraftsman/collection/concept/combo/scope.ws.kts
@@ -1,37 +1,55 @@
 package io.kraftsman.collection.concept.combo
 
-val fruits = mutableSetOf("Grape", "Muskmelon", "Kumquat", "Pear")
+val fruits = listOf("Grape", "Muskmelon", "Kumquat", "Pear")
 
 val parsedFruitResult = fruits.map { it.length }
     .filter { it > 4 }
+
 println(parsedFruitResult)
 
 fruits.map { it.length }
     .filter { it > 4 }
-    .let {
-        println(it)
-    }
+    .let { println(it) }
 
-val countEndsWithE = fruits.run {
+fruits.map { it.length }
+    .filter { it > 4 }
+    .let(::println)
+
+val fruitsSet = mutableSetOf("Grape", "Muskmelon", "Kumquat", "Pear")
+
+val countEndsWithE = fruitsSet.run {
     add("Papaya")
     add("Pineapple")
     count { it.endsWith("e") }
 }
+
 println("$countEndsWithE 個水果名稱以 e 結尾")
 
-val countStartWithP = with(fruits) {
-    add("Papaya")
-    add("Pineapple")
-    count { it.startsWith("P") }
-}
-println("$countStartWithP 個水果名稱以 P 開頭")
-
-fruits.apply {
-    add("Papaya")
-    add("Pineapple")
+with(fruits) {
+    println("清單裡有：$this")
+    println("總共有 $size 種水果")
 }
 
-fruits.map { it.length }
+val statistics = with(fruits) {
+    "清單裡的第一個水果是 ${first()}，" +
+    "清單裡的最後一個水果是 ${last()}"
+}
+
+println(statistics)
+
+val fruitsList = mutableListOf("Papaya", "Banana", "Orange")
+
+fruitsList.apply {
+    add("Apple")
+    add("Grape")
+}.sort()
+
+println(fruitsList)
+
+fruits.also(::println)
+    .map { it.length }
+    .also(::println)
     .filter { it > 4 }
-    .also { println(it) }
+    .also(::println)
     .take(2)
+    .also(::println)
