edd0248c16d44da1e9bcf0f77b174c2b41284804
Shengyou Fan <shengyoufan@gmail.com>
2022-05-31 14:55:45 +0800

    init Ktor project under practice folder

---
+++ src/main/kotlin/io/kraftsman/collection/practice/mock-server/src/main/kotlin/io/kraftsman/Application.kt
@@ -0,0 +1,12 @@
+package io.kraftsman
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.kraftsman.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
