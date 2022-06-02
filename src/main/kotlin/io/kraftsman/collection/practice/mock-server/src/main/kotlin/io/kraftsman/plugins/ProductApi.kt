65288288a7b45b3509629fa662ec6d8da68bbe47
Shengyou Fan <shengyoufan@gmail.com>
2022-06-02 17:27:34 +0800

    generate product data and return to client

--- src/main/kotlin/io/kraftsman/collection/practice/mock-server/src/main/kotlin/io/kraftsman/plugins/ProductApi.kt
+++ src/main/kotlin/io/kraftsman/collection/practice/mock-server/src/main/kotlin/io/kraftsman/plugins/ProductApi.kt
@@ -1,17 +1,60 @@
 package io.kraftsman.plugins
 
+import io.kraftsman.dtos.Meta
+import io.kraftsman.dtos.Product
+import io.kraftsman.dtos.ProductApiResponse
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlin.math.ceil
 
 fun Application.configureProductApi() {
 
+    val products = (1..50).map {
+        Product(
+            sku = "SKU-001",
+            name = "Product #$it",
+            description = "Product description",
+            price = 100,
+            rating = 3.2,
+            thumbnail = "https://api.lorem.space/image/furniture?w=300&h=300",
+            createdAt = "2022/06/01 00:00:00",
+            updatedAt = "2022/06/01 00:00:00",
+        )
+    }
+
     routing {
         get("/api/v1/products") {
+
+            val currentPage = 1
+            val total = products.count()
+            val perPage = 10
+            val lastPage = listOf(ceil(total.toDouble() / perPage).toInt(), 1).maxOrNull()!!
+
+            if (currentPage > lastPage) call.respond(
+                HttpStatusCode.NotFound,
+                mapOf("message" to "The requested page not found")
+            )
+
+            val index = currentPage - 1
+            val currentPageProduct = products.chunked(perPage)[index]
+            val from = index * perPage + 1
+            val to = from + currentPageProduct.count() - 1
+
             call.respond(
-                mapOf(
-                    "data" to "data",
-                    "meta" to "meta"
+                ProductApiResponse(
+                    data = currentPageProduct,
+                    meta = Meta(
+                        currentPage = currentPage,
+                        perPage = perPage,
+                        lastPage = lastPage,
+                        from = from,
+                        to = to,
+                        total = total,
+                    )
                 )
             )
         }
