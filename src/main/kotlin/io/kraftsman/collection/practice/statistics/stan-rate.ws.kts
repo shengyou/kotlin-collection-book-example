32c4a6ef45a64da2ec681b68734c1eaec5906f07
Shengyou Fan <shengyoufan@gmail.com>
2022-06-15 01:58:48 +0800

    optimized stan rate example

--- src/main/kotlin/io/kraftsman/collection/practice/statistics/stan-rate.ws.kts
+++ src/main/kotlin/io/kraftsman/collection/practice/statistics/stan-rate.ws.kts
@@ -5,25 +5,14 @@ import java.io.File
 val basePath = "/Users/shengyou/IdeaProjects/kotlin-collection-book-example/" +
         "src/main/kotlin/io/kraftsman/collection/practice/statistics/dataset"
 
-val registrantsPerEvents = File(basePath).listFiles { file -> file.extension == "csv" }
+File(basePath).listFiles { file -> file.extension == "csv" }
     ?.map { file ->
         file.readLines().drop(1).map { it.split(",")[0] }
     }
-
-val stanRate = registrantsPerEvents?.flatten()
-    ?.distinct()
-    ?.associate { it to 0 }
-    ?.toMutableMap()
-
-stanRate?.forEach { (registrant, _) ->
-    registrantsPerEvents?.forEach {
-        if (registrant in it) {
-            stanRate[registrant] = stanRate.getOrDefault(registrant, 0) + 1
-        }
-    }
-}
-
-stanRate?.toList()
+    ?.flatten()
+    ?.groupingBy { it }
+    ?.eachCount()
+    ?.toList()
     ?.sortedByDescending { it.second }
     ?.take(3)
     ?.toMap()
